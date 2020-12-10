f179df01cb8f49fa992bd0fc4b87f937793cb9c9
Develbold <st.siebold@gmail.com>
2020-12-10 21:54:56 +0100

    testing read analog

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,25 @@ int main()
 
     arduino = attachUUGearDevice ((char *)"UUGear-Arduino-7853-2668");
 
+    while(true)
+    {
+        if(arduino.fd != -1)
+        {
+            int pin0 = 0;	// analog input pin 3
+            int pin1 = 1;	// analog input pin 3
+
+            int value0,value1;
+            float voltage0,voltage1;
+            value0 = analogRead(&arduino, pin0);
+            value1 = analogRead(&arduino, pin1);
+
+            voltage0 = (float)(value0 * 5) / 1024;
+            voltage1 = (float)(value0 * 5) / 1024;
+
+            printf("%.2fV and %.2fV\n", voltage0,voltage1);
+        }
+    }
+
     cout << "INFO: finished execution" << endl;
     return 0;
 }
