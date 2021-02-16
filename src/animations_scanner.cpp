acf57d2534cf6d022e1c60a6f47ac5627bdc7a06
Develbold <st.siebold@gmail.com>
2021-02-16 21:44:47 +0100

    ADDED: front bar

--- src/animations_scanner.cpp
+++ src/animations_scanner.cpp
@@ -17,24 +17,29 @@ ANIMATION_SCANNER::~ANIMATION_SCANNER()
 void ANIMATION_SCANNER::render(fades_t direction)
 {
     auto pos_dot = 0;
-    auto pos_bar = getLEDCount();
-    while(pos_bar!=0)
+    auto pos_bar_back = getLEDCount();
+    auto pos_bar_front = 0;
+    while(pos_bar_back!=0)
     {
         // set new postition
         setOneLED(pos_dot,cMax_brightness_);
-        // set old pos to zero
-        setOneLED(pos_dot-1,0);
-        // check if dot is same pos as bar
-        if(pos_dot==pos_bar)
+        // set old pos to zero if it doesnt delete front bar
+        if(pos_dot-1>pos_bar_front)
         {
-            pos_bar = pos_dot-1;
-            pos_dot = 0;
+            setOneLED(pos_dot-1,0);
+        }
+        // check if dot is same pos as bar and update positions
+        if(pos_dot==pos_bar_back)
+        {
+            pos_bar_back = pos_dot-1;
+            pos_dot = pos_bar_front;
+            pos_bar_front++;
         }
         // render LEDs
         renderLEDs();
         //update postition
         pos_dot++;
-        BOOST_LOG_TRIVIAL(debug) << "positions: dot=" << pos_dot << ", pos_bar=" << pos_bar;
+        BOOST_LOG_TRIVIAL(debug) << "positions: dot=" << pos_dot << ", pos_bar=" << pos_bar_back << ", pos_bar_front=" << pos_bar_front;
     }
 }
 
