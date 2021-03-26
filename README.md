f81e798d1a04a8a4716ce0fee3fde4495223ef99
Develbold <st.siebold@gmail.com>
2021-03-26 17:51:38 +0100

    added paho include

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flurlicht
+
+
+## Requirements
+- compiled and installed Paho c++ (https://github.com/eclipse/paho.mqtt.cpp)
---
+++ include/flurlicht_mqtt.h
@@ -0,0 +1,13 @@
+#ifndef FLURLICHT_MQTT_H
+#define FLURLICHT_MQTT_H
+
+
+class FLURLICHT_MQTT
+{
+public:
+    FLURLICHT_MQTT();
+    ~FLURLICHT_MQTT();
+    bool run();
+};
+
+#endif // FLURLICHT_MQTT_H
