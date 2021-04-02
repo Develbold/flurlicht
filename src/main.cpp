57aa1f722104d38afcf5f4ba874f5a9f356e4d73
Develbold <st.siebold@gmail.com>
2021-04-02 13:32:06 +0200

    refactored MQTT class

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "flurlicht.h"
+#include "flurlicht_mqtt.h"
 #include <iostream>
 
 auto main() -> int
 {
     BOOST_LOG_TRIVIAL(info) << "Flurlicht started";
 
-    flurlicht foo;
-    foo.run();
+//    flurlicht foo;
+//    foo.run();
+    FLURLICHT_MQTT mqtt;
+    mqtt.run();
 
     std::cout << "INFO: finished execution" << std::endl;
     return 0;
