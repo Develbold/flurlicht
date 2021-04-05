9765ca144073654f8471a1afed87f4d948d0a2b1
Develbold <st.siebold@gmail.com>
2021-04-05 18:22:40 +0200

    fix: missed files

---
+++ include/flurlicht_occupancy.h
@@ -0,0 +1,19 @@
+#ifndef FLURLICHT_EVENTS_H
+#define FLURLICHT_EVENTS_H
+
+#include <boost/thread/mutex.hpp>
+
+class FLURLICHT_OCCUPANCY
+{
+public:
+    FLURLICHT_OCCUPANCY();
+    ~FLURLICHT_OCCUPANCY();
+    void resetTrigger();
+    bool getOccupancy();
+private:
+    boost::mutex lock_;
+    std::chrono::high_resolution_clock::time_point last_trigger_time_;
+    const int cOccupancyTime_ = 5000;
+};
+
+#endif // FLURLICHT_EVENTS_H
---
+++ src/flurlicht_occupancy.cpp
@@ -0,0 +1,31 @@
+#include "flurlicht_occupancy.h"
+#include "flurlicht_tools.h"
+#include <boost/log/trivial.hpp>
+
+FLURLICHT_OCCUPANCY::FLURLICHT_OCCUPANCY()
+{
+    BOOST_LOG_TRIVIAL(debug) << "FLURLICHT_OCCUPANCY::FLURLICHT_OCCUPANCY";
+}
+
+FLURLICHT_OCCUPANCY::~FLURLICHT_OCCUPANCY()
+{
+    BOOST_LOG_TRIVIAL(debug) << "FLURLICHT_OCCUPANCY::~FLURLICHT_OCCUPANCY";
+}
+
+// set last trigger time to now
+void FLURLICHT_OCCUPANCY::resetTrigger()
+{
+    BOOST_LOG_TRIVIAL(debug) << "OCCUPANCY: reset trigger";
+    lock_.lock();
+    last_trigger_time_ = FLURLICHT_TOOLS::getTime();
+    lock_.unlock();
+}
+
+// if last time triggered is bigger then occupancy time, set occupancy to true
+bool FLURLICHT_OCCUPANCY::getOccupancy()
+{
+    lock_.lock();
+    bool buffer = FLURLICHT_TOOLS::checkRenderTimeValid(last_trigger_time_,cOccupancyTime_);
+    lock_.unlock();
+    return buffer;
+}
