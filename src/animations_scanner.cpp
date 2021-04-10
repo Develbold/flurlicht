fc643472c40799941899023b8906313749863ac8
Develbold <st.siebold@gmail.com>
2021-04-10 15:04:11 +0200

    fixed typo

--- src/animations_scanner.cpp
+++ src/animations_scanner.cpp
@@ -22,9 +22,9 @@ void ANIMATION_SCANNER::render(fades_t direction)
     auto pos_bar_front = 0;
     while(pos_bar_front!=pos_bar_back)
     {
-        // set new postition
+        // set new position
         setOneLED(pos_dot,cMax_brightness_);
-        // set old pos to zero if it doesnt delete front bar
+        // set old pos to zero if it doesn't delete front bar
         if(pos_dot-1>pos_bar_front)
         {
             setOneLED(pos_dot-1,0);
@@ -38,7 +38,7 @@ void ANIMATION_SCANNER::render(fades_t direction)
         }
         // render LEDs
         renderLEDs();
-        //update postition
+        //update position
         pos_dot++;
 //        BOOST_LOG_TRIVIAL(debug) << "positions: dot=" << pos_dot << ", pos_bar=" << pos_bar_back << ", pos_bar_front=" << pos_bar_front;
     }
