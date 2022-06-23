d73c4cc33104925554b4d29da207cd55e6cd8c04
Develbold <st.siebold@gmail.com>
2022-06-23 15:25:43 +0200

    updated home-assistant ids

--- include/flurlicht_mqtt.h
+++ include/flurlicht_mqtt.h
@@ -15,11 +15,11 @@ public:
     static bool parsePayload(std::string msg);
 
 private:
-    const std::string SERVER_ADDRESS = "tcp://192.168.0.12:1883";
+    const std::string SERVER_ADDRESS = "tcp://192.168.0.3:1883";
     const std::string CLIENT_ID = "paho_cpp_async_subcribe";
     const std::string USER = "arduino";
     const std::string PW = "foobar";
-    const std::vector<std::string> TOPICS = {"homeassistant/binary_sensor/0010fa6e384a/pir_front/state","homeassistant/binary_sensor/0010fa6e384a/pir_back/state"};
+    const std::vector<std::string> TOPICS = {"homeassistant/binary_sensor/pir_front/state","homeassistant/binary_sensor/pir_back/state"};
     static const int	QOS = 1;
     static const int	N_RETRY_ATTEMPTS = 5;
 
